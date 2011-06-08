6523f43fa99c0cd27a652a6ccb23fb342f85a894
Gorka Hernandez <gorka91@gmail.com>
2011-06-08 17:26:05 +0200

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
+  gem 'annotate-models', '1.0.4'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
   def new
     @title = "Sign up"
   end
