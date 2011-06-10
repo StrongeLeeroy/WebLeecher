e5bc6aa3b1bcf60ba23880c9863df253a19d9487
Gorka Hernandez <gorka91@gmail.com>
2011-06-10 18:11:41 +0200

    Some styling

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -14,7 +14,6 @@ class SearchesController < ApplicationController
   end
 
   def create
-   
     username = params[:search][:forumuser]
     password = params[:search][:forumpass]
     forumchoice = params[:search][:forumchoice]
@@ -34,107 +33,114 @@ class SearchesController < ApplicationController
     login_form['cookieuser'] = "1"
     ### Submit the login form ###
     page = agent.submit login_form
-    pagetitle = agent.page.title()
 
-    page=agent.get("http://tehparadox.com/forum/search.php")
+    page = agent.get("http://tehparadox.com/forum/search.php")
     @token=""
-    ### Load the whole page ###
-    troll = page.parser.xpath('/html').map do |row|
-      test = row.text.to_s
-      if test =~ /SECURITY/
-          File.open('temp.txt','w') do |f|
-            f.puts test
-          end
-          File.open('temp.txt', 'r') do |f|
-            while line = f.gets
-              if line =~ /SECURITY/
-			          @token = line
+      ### Load the whole page ###
+      troll = page.parser.xpath('/html').map do |row|
+        test = row.text.to_s
+        if test =~ /SECURITY/
+            File.open('temp.txt','w') do |f|
+              f.puts test
+            end
+            File.open('temp.txt', 'r') do |f|
+              while line = f.gets
+                if line =~ /SECURITY/
+			            @token = line
+                end
               end
             end
+        end     
+      ### Trim the string to just the actual token ###
+	    @token = @token.scan(/\"([^.]+)\"/).to_s
+      end
+
+    
+    securitytoken = @token
+
+    if securitytoken != "guest"
+      ### Load the search form ###
+      search_form = agent.page.form_with(:action => 'search.php?do=process')
+      ### Populate the search form ###
+      childforums2 = 1
+      prefixchoice2 = "MU"
+
+
+      ### Complete search form with data and token ###
+      search_form['query'] = query
+      search_form['titleonly'] = 1
+      search_form['forumchoice[]'] = forumchoice
+      search_form['childforums'] = childforums2
+      search_form['prefixchoice[]'] = prefixchoice2
+      search_form['s'] = ""
+      search_form['securitytoken'] = securitytoken
+      search_form['do'] = "process"
+      search_form['searchthreadid'] = ""
+      ### submit the search form ###
+      page=agent.submit search_form
+      ### parse all the threads into a temporary txt ###
+      threadmen = page.parser.xpath('//*[@id="threadslist"]').map do |row|
+	      if row.text =~ /[MU]/
+		      File.open('temp.txt', 'w') do |f|
+			      f.puts row
+		      end
+	      end
+      end
+
+      ### Make sure that "threads.txt" exists ###
+      File.open("threads.txt",'w') do |nothing|
+      end
+      ### Get the thread titles from the file ###
+      File.open('temp.txt', 'r') do |f2|
+        while line = f2.gets
+          if line =~ /thread_title/
+              File.open('threads.txt', 'a') do |threads|
+                threads.puts line
+              end
           end
-      end
-      
-    ### Trim the string to just the actual token ###
-	  @token = @token.scan(/\"([^.]+)\"/).to_s
-    end
-    securitytoken=@token
-
-    ### Load the search form ###
-    search_form = agent.page.form_with(:action => 'search.php?do=process')
-    ### Populate the search form ###
-    childforums2 = 1
-    prefixchoice2 = "MU"
-
-
-    ### Complete search form with data and token ###
-    search_form['query'] = query
-    search_form['titleonly'] = 1
-    search_form['forumchoice[]'] = forumchoice
-    search_form['childforums'] = childforums2
-    search_form['prefixchoice[]'] = prefixchoice2
-    search_form['s'] = ""
-    search_form['securitytoken'] = securitytoken
-    search_form['do'] = "process"
-    search_form['searchthreadid'] = ""
-    ### submit the search form ###
-    page=agent.submit search_form
-    ### parse all the threads into a temporary txt ###
-    threadmen = page.parser.xpath('//*[@id="threadslist"]').map do |row|
-	    if row.text =~ /[MU]/
-		    File.open('temp.txt', 'w') do |f|
-			    f.puts row
-		    end
-	    end
-    end
-
-    ### Make sure that "threads.txt" exists ###
-    File.open("threads.txt",'w') do |nothing|
-    end
-    ### Get the thread titles from the file ###
-    File.open('temp.txt', 'r') do |f2|
-      while line = f2.gets
-        if line =~ /thread_title/
-            File.open('threads.txt', 'a') do |threads|
-              threads.puts line
-            end
         end
       end
-    end
 
-    ### Create and initialize the array ###
-    i = 1
-    @threadlist = arraymen(50,50)
-    @threadlist[0][0] = nil
+      ### Create and initialize the array ###
+      i = 1
+      @threadlist = arraymen(50,50)
+      @threadlist[0][0] = nil
 
-    ### Fill the array with the titles and links from the file ###
-    File.open("threads.txt", 'r') do |threads|
-      while line = threads.gets
-	      if @OS==1 ## WINDOWS ##
-		      title=line.scan(/\>([^*]+)\</).to_s
-		      link=line.scan(/"f([^.]+)\?/).to_s
-		      var=title.length - 6
-		      title=title[3,var]
-		      var=link.length - 6
-		      link=link[3,var]
-        else
-		      title=line.scan(/\>([^*]+)\</)
-		      link=line.scan(/"f([^.]+)\?/)
+      ### Fill the array with the titles and links from the file ###
+      File.open("threads.txt", 'r') do |threads|
+        while line = threads.gets
+	        if @OS==1 ## WINDOWS ##
+		        title=line.scan(/\>([^*]+)\</).to_s
+		        link=line.scan(/"f([^.]+)\?/).to_s
+		        var=title.length - 6
+		        title=title[3,var]
+		        var=link.length - 6
+		        link=link[3,var]
+          else
+		        title=line.scan(/\>([^*]+)\</)
+		        link=line.scan(/"f([^.]+)\?/)
+          end
+		      @threadlist[i][0]=title # scans for thread title
+		      @threadlist[i][1]="http://tehparadox.com/forum/f#{link}"
+	        i = i + 1
         end
-		    @threadlist[i][0]=title # scans for thread title
-		    @threadlist[i][1]="http://tehparadox.com/forum/f#{link}"
-	      i = i + 1
       end
+
+
+      @searchquery = { :username => username, :password => password, :forumchoice => forumchoice, :query => query }
+      render 'update'
+    else
+      flash.now[:error] = "Login data was incorrect."
+      @title = "Le Leecher"
+      render 'new'
     end
-
-
-    @searchquery = { :username => username, :password => password, :forumchoice => forumchoice, :query => query }
-    render 'update'
   end
 
 
+
   def update
-    username = "" #params[:search][:forumuser]
-    password = "" #params[:search][:forumpass]
+    username = "Lurkerman" #params[:search][:forumuser]
+    password = "tetillas" #params[:search][:forumpass]
     digest = Digest::MD5.hexdigest(password)
     agent = Mechanize.new
     page = agent.get("http://tehparadox.com/forum/index.php")
